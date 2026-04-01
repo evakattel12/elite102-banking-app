dedd4f2b8febb05cc2acf5b3ab0338dc3ae60b78
evakattel12 <eva.kattel.12@gmail.com>
2026-04-01 21:41:08 +0000

    Basic database bank_accounts created with id, name, account type, and balance. Used DROP, CREATE, INSERT, and VALUES to make the rows and table in SQLite3.

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import sqlite3
 con = sqlite3.connect("tutorial.db")
 cur = con.cursor()
-cur.execute("DROP TABLE test")
+cur.execute("CREATE TABLE bank_accounts(id, name, account, balance)")
+cur.execute("""INSERT INTO bank_accounts VALUES
+            (1, 'Marie Grace', 'Checking', 500),
+            (2, 'Jose Garcia', 'Savings', 300)
+            """)
+con.commit()
